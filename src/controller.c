7facee000ce351fe39baf69d0244e2de6c97904d
Adrian Senn <adu.senn@hotmail.com>
2016-03-30 13:29:26 +0200

    test update

--- src/controller.c
+++ src/controller.c
@@ -5,4 +5,21 @@
  *      Author: Adrian Senn
  */
 
+/*----- Header-Files --------------------- ---------------------------------*/
+#include <stdio.h>					/* Standard input and output			*/
+#include <stdlib.h>					/* Standard library        		        */
+
+#include <stm32f4xx.h>				/* Processor STM32F407IG				*/
+#include <carme.h>					/* CARME Module							*/
+#include <uart.h>					/* CARME BSP UART port					*/
+#include <can.h>					/* CARME BSP CAN  port					*/
+#include <lcd.h>
+#include <carme_io1.h>
+#include <carme_io2.h>
+
+#include <FreeRTOS.h>				/* FreeRTOS								*/
+#include <task.h>					/* FreeRTOS tasks						*/
+#include <queue.h>					/* FreeRTOS queues						*/
+#include <semphr.h>					/* FreeRTOS semaphores					*/
+#include <memPoolService.h>			/* Memory pool manager service			*/
 
