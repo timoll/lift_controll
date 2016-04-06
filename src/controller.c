1f7c60d441d0f2456da93ed4021fd765a099e248
Adrian Senn <adu.senn@hotmail.com>
2016-04-06 13:27:03 +0200

    update controllerfile

--- src/controller.c
+++ src/controller.c
@@ -22,4 +22,102 @@
 #include <queue.h>					/* FreeRTOS queues						*/
 #include <semphr.h>					/* FreeRTOS semaphores					*/
 #include <memPoolService.h>			/* Memory pool manager service			*/
+/*----- Data ---------------------------------------------------------------*/
+char Pending_orders [20][3];
+char Jobs_inprogress_lift_1 [10][2];
+char Jobs_inprogress_lift_2 [10][2];
 
+char possible_floors_lift_1[2];
+char possible_floors_lift_2[2];
+
+char last_position_lift_1;
+char last_position_lift_2;
+char direction_lift_1;
+char direction_lift_2;
+
+char state=0;
+
+/*----- Definition ----------------------------------------------------------*/
+#define Inform				0
+#define Order_distribution	1
+#define Inquiry				2
+#define Wait				3
+#define New_information		4
+/*----- Function prototypes ------------------------------------------------*/
+
+char maxmin_floor (char direction,char position);
+char Direction (char p[][2],char last_position);
+
+void controller(void)
+{
+	//wichtige informationen updaten
+	switch (state)
+	{
+		case Inform:
+
+			direction_lift_1=Direction(Jobs_inprogress_lift_1,last_position_lift_1);
+			direction_lift_2=Direction(Jobs_inprogress_lift_2,last_position_lift_2);
+			possible_floors_lift_1[0]=last_position_lift_1;
+			possible_floors_lift_2[0]=last_position_lift_2;
+			possible_floors_lift_1[1]=maxmin_floor(direction_lift_1,last_position_lift_1);
+			possible_floors_lift_2[1]=maxmin_floor(direction_lift_2,last_position_lift_2);
+
+
+			break;
+
+		case Order_distribution:
+
+			break;
+
+		case Inquiry:
+
+			break;
+
+		case Wait:
+
+			break;
+
+		case New_information:
+
+			break;
+
+	}
+
+}
+
+char Direction (char p[][2],char last_position)
+{
+	if(last_position==p[0][0])
+	{
+		return 0;
+	}else if(last_position>p[0][0])
+	{
+		return 2;
+	}else if(last_position<p[0][0])
+	{
+		return 1;
+	}else
+	{
+		return 0;
+	}
+	// höchste oder tiefste position
+}
+
+char Array_arrange (void) //Array sortieren
+{
+
+}
+
+char maxmin_floor (char direction,char position)
+{
+	if(direction==0)
+	{
+		return position;
+	}else if(direction==1)
+	{
+		return 5;
+	}else
+	{
+		return 1;
+	}
+}
